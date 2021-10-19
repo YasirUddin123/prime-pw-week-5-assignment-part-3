bc13ef53167565fb9666649c32b75ec19af50e37
Yasir Uddin <yasir.uddin@icloud.com>
2021-10-19 16:05:25 -0700

    created an empty array called collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+
+// REQUIRED FEATURES
+
+//#1 - Create a variable called collection that starts as an empty array.
+
+    //CODE
+    let collection = [];
+
+//#2
